81afdd014679b2358b563a4605b31527ce10afb4
jacobwaurand <59833365+jacobwaurand@users.noreply.github.com>
2020-03-05 22:51:44 -0500

    Merge branch 'master' of C:\xampp\htdocs\SearchCommittee\medical with conflicts.

--- applicant.controller.js
+++ applicant.controller.js
@@ -34,34 +34,12 @@ angular.module('myApplicant')
                 .then(function (result) {
                     $scope.documents = result.data;
                 }, function () {
-                    alert("Error Finding Application Files");
+                    alert("Error deleting records");
                 });
-
-            var request = {
-                method: "get",
-                url: './php/search-committee_getAppDocs.php',
-                dataType: "json",
-                headers: {"Content-Type": "application/x-www-form-urlencoded"}
-            };
-
-            $scope.completedDocuments = [];
-
-            $http(request)
-                .then(function (result) {
-                    $scope.completedDocuments = result.data;
-                }, function () {
-                    alert("Error Finding App Docs");
-                });
-            // let content = "x";
-            $scope.checkCompletedDocuments = function checkCompletedDocument(applicantID, doc){
-                angular.forEach($scope.completedDocuments, function(compdocument) {
-                    if(applicantID === compdocument.fkApplicantID && doc === compdocument.fkApplicationFileID) {
-                        document.getElementById(applicantID + " " + doc).innerHTML="&#10003";
-                        document.getElementById(applicantID + " " + doc).parentElement.className="color-green";
-                    }
-                });
-            }
         }
+
+
+
 
     ]);
 

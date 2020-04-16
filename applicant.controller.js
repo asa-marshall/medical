284127018804c402571d8a42563b37f59975d5c1
Asa Marshall <asam268@gmail.com>
2020-04-16 19:07:31 -0400

    Added column for completed requirements. This column shows a check mark when at least one required document has been filled out. Needs to only show a check mark when all documents have been filled out.

--- applicant.controller.js
+++ applicant.controller.js
@@ -52,14 +52,32 @@ angular.module('myApplicant')
                 }, function () {
                     alert("Error Finding App Docs");
                 });
-
+            // let total = 0;
             $scope.checkCompletedDocuments = function checkCompletedDocument(applicantID, doc) {
+                // let total = 0;
+
                 angular.forEach($scope.completedDocuments, function (compdocument) {
+                    // alert("loop");
                     if (applicantID === compdocument.fkApplicantID && doc === compdocument.fkApplicationFileID) {
                         document.getElementById(applicantID + " " + doc).innerHTML = "&#10003";
                         document.getElementById(applicantID + " " + doc).parentElement.className = "green";
+                        angular.forEach($scope.documents, function (rdocument) {
+                            if(rdocument.IsRequired === "1" && compdocument.fkApplicationFileID === rdocument.AutoID){
+                                // total++;
+                                // alert(compdocument.fkApplicantID + " " + rdocument.Description + " with total " + total);
+                                // if (total > 1){
+                                    document.getElementById(applicantID + " isRequired").innerHTML = "&#10003";
+                                    document.getElementById(applicantID + " isRequired").parentElement.className = "green";
+                                // }
+                            }
+                        })
                     }
                 });
+                // total = 0;
+            };
+
+            $scope.checkCompletedRequirements = function checkCompletedRequirements(applicantID, isRequired) {
+
             }
         }
 
